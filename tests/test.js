e44d05a9b10809282d96e5c807ec8201f0818e8a
Orionlight231 <orionlight231@gmail.com>
2018-02-12 20:17:27 +0100

    add 2 function

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,57 @@ var test = require('tape');
 var logic = require('../logic');
 
 test('Example test', function(t) {
-  t.pass();
+  var existingTodos = [];
+  var todos = logic.addTodo(existingTodos, "324");
+  var expectedTodos = [
+    {
+    id: 1,
+    description: '324',
+    done: false,
+    },
+  ];
+  t.deepEquals(todos, expectedTodos);
+
+  var todosList = logic.addTodo(todos, "1234");
+  var expectedTodosList = [
+    {
+    id: 1,
+    description: '324',
+    done: false,
+    },
+    {
+    id: 2,
+    description: '123',
+    done: false,
+    },
+  ];
+  t.deepEquals(todosList, expectedTodosList);
+  t.end();
+});
+
+test('Delete test', function(t) {
+  var existingTodos = [
+    {
+    id: 1,
+    description: '324',
+    done: false,
+    },
+    {
+    id: 2,
+    description: '1234',
+    done: false,
+    },
+  ];
+  var todos = logic.deleteTodo(existingTodos, 1);
+  var expectedTodos = [
+    {
+    id: 2,
+    description: '1234',
+    done: false,
+    },
+  ];
+  t.deepEquals(todos, expectedTodos);
+
+
   t.end();
 });
